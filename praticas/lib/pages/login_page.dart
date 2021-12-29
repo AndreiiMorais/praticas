816b3ce3bcf98efaf9048f6dd4dc96ad125bf20c
Andrei Silveira Morais <andrei.morais@outlook.com>
2021-12-28 21:23:38 -0300

    created LoginController

--- praticas/lib/pages/login_page.dart
+++ praticas/lib/pages/login_page.dart
@@ -1,9 +1,12 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:praticas/controllers/login_controller.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  LoginController _controller = LoginController();
+
+  LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,11 +23,13 @@ class LoginPage extends StatelessWidget {
               TextField(
                 decoration: InputDecoration(hintText: 'Login'),
                 textCapitalization: TextCapitalization.words,
+                onChanged: _controller.setLogin,
               ),
               SizedBox(height: 20),
               TextField(
                 decoration: InputDecoration(hintText: 'Password'),
                 obscureText: true,
+                onChanged: _controller.setPass,
               ),
               SizedBox(height: 10), //tambem é possivel usar o Spacer
               ElevatedButton(
